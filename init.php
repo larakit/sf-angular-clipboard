fba770715834d6c735eff13323060c7b21811e92
Alexey Berdnikov <aberdnikov@gmail.com>
2017-09-05 18:32:31 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-clipboard')
+    ->setSourceDir('public')
+    ->usePackage('larakit/sf-angular')
+    ->jsPackage('angular-clipboard.js');
